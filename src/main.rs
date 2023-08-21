f0b55d41263f37d6b8830d59ce3b8cbc43f2c563
motii <108280115+motii8128@users.noreply.github.com>
2023-08-21 11:58:01 +0900

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use safe_drive::{context::Context, error::DynError, logger::Logger, pr_info, msg::common_interfaces::geometry_msgs};
+use std::time::Duration;
+
+fn main()->Result<(), DynError>
+{
+    let context = Context::new()?;
+
+    let node = context.create_node("turtle_test", None, Default::default())?;
+
+    let publisher = node.create_publisher::<geometry_msgs::msg::Twist>("turtle1/cmd_vel", None)?;
+
+    let logger = Logger::new("turtle_test");
+
+    let mut msg = geometry_msgs::msg::Twist::new().unwrap();
+
+    let mut value = 1.0;
+
+    loop
+    {
+        msg.linear.x = value;
+
+        pr_info!(logger, "kame ugoke");
+        publisher.send(&msg)?;
+
+        value = value * -1.0;
+
+        std::thread::sleep(Duration::from_millis(1000));
+    }
+}
